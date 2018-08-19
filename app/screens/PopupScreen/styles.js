55f254e735203f1119741927c17d7a7d02cb49c8
Imron Rosdiana <imron.r@meteor.id>
2018-08-19 16:20:45 +0700

    Add popup screen

---
+++ app/screens/PopupScreen/styles.js
@@ -0,0 +1,26 @@
+// @flow
+
+import { StyleSheet } from 'react-native';
+import colors from '../../utils/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent'
+  },
+  buttonText: {
+    fontSize: 16,
+    color: colors.BLACK,
+    fontWeight: 'normal'
+  },
+  bodyText: {
+    fontSize: 18
+  }
+});
+
+export default styles;
--- app/utils/colors.js
+++ app/utils/colors.js
@@ -3,7 +3,9 @@
 export default {
   PRIMARY: '#2196F3',
   BLACK: '#000000',
+  BLACK_TRANSPARENT: 'rgba(0, 0, 0, 0.5)',
   WHITE: '#FFFFFF',
   RED: '#F44336',
-  GREY: '#9E9E9E'
+  GREY: '#9E9E9E',
+  DOGGER_BLUE: '#068CEF'
 };
