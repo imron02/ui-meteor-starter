c7f240acc7c33d5c2fbc2e8321d7e7f2fd86f5c8
Imron Rosdiana <imron.r@meteor.id>
2018-08-19 16:19:03 +0700

    Add popup component

---
+++ app/components/Popup/props.js
@@ -0,0 +1,11 @@
+// @flow
+
+export type Props = {
+  visible: boolean;
+  headerStyle?: string | number;
+  headerTitle: string;
+  headerTitleStyle?: string | number;
+  headerImage?: string;
+  bodyStyle?: string | number;
+  onPress: () => void;
+};
