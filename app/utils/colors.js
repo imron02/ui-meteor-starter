f2ef8f0591dc9abf3c7c0466ac4252b943ea88c7
Imron Rosdiana <imron.r@meteor.id>
2018-08-21 21:56:59 +0700

    Add button color base

--- app/utils/colors.js
+++ app/utils/colors.js
@@ -1,8 +1,57 @@
 // @flow
 
+const PRIMARY = '#2196F3';
+const SUCCESS = '#67AA56';
+const INFO = '#639FEC';
+const WARNING = '#E1A151';
+const DANGER = '#BF4744';
+const BLACK = '#000000';
+
+const BUTTON_STYLE = {
+  borderWidth: 1,
+  borderRadius: 2,
+  elevation: 2,
+  alignItems: 'center',
+  justifyContent: 'center'
+};
+
+const BUTTON_COLOR = {
+  primary: {
+    backgroundColor: PRIMARY,
+    borderColor: PRIMARY,
+    ...BUTTON_STYLE
+  },
+  success: {
+    backgroundColor: SUCCESS,
+    borderColor: SUCCESS,
+    ...BUTTON_STYLE
+  },
+  info: {
+    backgroundColor: INFO,
+    borderColor: INFO,
+    ...BUTTON_STYLE
+  },
+  warning: {
+    backgroundColor: WARNING,
+    borderColor: WARNING,
+    ...BUTTON_STYLE
+  },
+  danger: {
+    backgroundColor: DANGER,
+    borderColor: DANGER,
+    ...BUTTON_STYLE
+  },
+  black: {
+    backgroundColor: BLACK,
+    borderColor: BLACK,
+    ...BUTTON_STYLE
+  }
+};
+
 export default {
-  PRIMARY: '#2196F3',
-  BLACK: '#000000',
+  button: BUTTON_COLOR,
+  PRIMARY,
+  BLACK,
   BLACK_TRANSPARENT: 'rgba(0, 0, 0, 0.5)',
   DUSTY_GRAY: '#999999',
   WHITE: '#FFFFFF',
@@ -10,5 +59,9 @@ export default {
   GREY: '#9E9E9E',
   SILVER_CHALICE: '#A3A3A3',
   GRAY: '#EEEEEE',
-  DODGER_BLUE: '#068CEF'
+  DODGER_BLUE: '#068CEF',
+  SUCCESS,
+  INFO,
+  WARNING,
+  DANGER
 };
